f5c7ef0353c2d37364cd6ce4a782852a65e60f91
taq-f <mysecondproject@gmail.com>
2018-08-15 12:55:07 +0900

    mongodb connection pooling

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,26 +1,32 @@
 import { MongoClient } from 'mongodb'
 
 const DATABASE_URI = 'mongodb://127.0.0.1:27017'
+let db
+
+async function getDb() {
+  if (!db) {
+    let client
+    try {
+      client = await MongoClient.connect(DATABASE_URI, { useNewUrlParser: true })
+      db = client.db('images')
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+  return db
+}
 
 export async function getAll() {
-  let client
-  try {
-    client = await MongoClient.connect(DATABASE_URI, { useNewUrlParser: true })
-    const db = client.db('images')
-    const collection = db.collection('images')
-    const images = await collection.find(
-      {},
-      {
-        projection: {
-          name: 1,
-        },
+  const db = await getDb()
+  const collection = db.collection('images')
+  const images = await collection.find(
+    {},
+    {
+      projection: {
+        name: 1,
       },
-    ).toArray()
+    },
+  ).toArray()
 
-    return images
-  } catch (error) {
-    throw new Error(error)
-  } finally {
-    if (client) client.close()
-  }
+  return images
 }
